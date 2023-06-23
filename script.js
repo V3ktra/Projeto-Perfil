399fe68adb755bf2a0ab824bb4bf63613847438f
Mariana Akemi <mariana.akemi.nakamura@gmail.com>
2023-06-23 19:01:49 -0300

    finalização do projeto perfil

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 alert("Olá Seja Bem-vindo")
 
 let p1=prompt("Digite Seu NOME"); 
-let p2=prompt("Digite um PODER que gostaria de ter"); 
-let p3=prompt("Digite o nome de um VILÃO"); 
-let p4=prompt("Digite um LUGAR que gostaria de estar"); 
+let p2=prompt("Digite, qual player você seria?"); 
+let p3=prompt("Digite, onde você moraria?"); 
+let p4=prompt("Digite, qual ovo você cuidaria?"); 
 
 let msg= document.getElementById('msg'); 
 
-msg.innerHTML=`<p>Olá ${p1} o seu poder é a/o ${p2} você vau enfrentar o/a ${p3} no/em ${p4}`
+msg.innerHTML=`<p>Olá ${p1} o seu você seria ${p2} você vai morar na(o) ${p3} cuidando de ${p4}`
