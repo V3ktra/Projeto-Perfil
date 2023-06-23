6c6922f9c77dc719eeb1aa88425935b4539bb0f3
Mariana Akemi <mariana.akemi.nakamura@gmail.com>
2023-06-23 18:27:13 -0300

    Corrigindo mais erros

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-alert("Olá Seja Bem-vindo")/*alerta mostranso no início Seja Bem-vindo*/
-let p1=prompt("Digite Seu NOME"); /*declarando a variável p1 como let e chamando prompt para que você digite o nome*/
-let p2=prompt("Digite um PODER que gostaria de ter"); /*declarando a variável p2 como let e chamando prompt para que você digite o poder*/
-let p3=prompt("Digite o nome de um VILÃO"); /*declarando a variável p3 como let e chamando prompt para que você digite o vilão*/
-let p4=prompt("Digite um LUGAR que gostaria de estar"); /*declarando a variável p4 como let e chamando prompt para que você digite o lugar*/
-let msg= document.getElementById('msg'); /*declarando a variável msg e chamando o id criado dentro do <p> no html*/
+alert("Olá Seja Bem-vindo")
 
-/* para chamar o id msd e mostrar na tela precisamos criar msg em seguida o innerHTML (mostre no html)
-'' a crase é uma estrutura na nova versão do javascript chamada TEMPLAIT STRINGS
-$ representa a string
-{} onde é passado a variável que deseja manipular como concatenar, como no exemplo abaixo */
+let p1=prompt("Digite Seu NOME"); 
+let p2=prompt("Digite um PODER que gostaria de ter"); 
+let p3=prompt("Digite o nome de um VILÃO"); 
+let p4=prompt("Digite um LUGAR que gostaria de estar"); 
 
-msg;innerHTML=<p>Olá ${p1} o seu poder é a/o ${p2} você vau enfrentar o/a ${p3} no/em ${p4}</p>
+let msg= document.getElementById('msg'); 
+
+msg.innerHTML=`<p>Olá ${p1} o seu poder é a/o ${p2} você vau enfrentar o/a ${p3} no/em ${p4}`
